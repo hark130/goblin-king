1965922924f3edba17b5064b1ff4d7ba34a28009
hark130 <hark130@yahoo.com>
2022-06-12 13:18:59 -0500

    GOKI-1: Updated goki/goki.py
    - Refactored to take advantage of get_choice()'s clear_screen feature.
    - Wrapped user response in try/catch
    - Tentatively implemented call to new rando_db_entr*() functions
    
    Changes to be committed:
    	modified:   goki/goki.py

--- goki/goki.py
+++ goki/goki.py
@@ -3,9 +3,10 @@
 # Standard Imports
 # Third Party Imports
 # Local Imports
-from goki.constants import MAIN_MENU
+from goki.constants import EQUIP_FOOD_DB, MAIN_MENU
 from goki.menu import get_choice
 from goki.misc import print_exception
+from goki.rando import rando_db_entries, rando_db_entry
 
 
 def main() -> int:
@@ -18,14 +19,16 @@ def main() -> int:
         3 on unspecified error (e.g., ?!?!?!)
     """
     # LOCAL VARIABLES
-    ret_val = 0     # Return value: 0 success, 1 bad env, 2 bad input
-    user_input = 0  # Main menu user choice
+    ret_val = 0          # Return value: 0 success, 1 bad env, 2 bad input
+    clear_screen = True  # Controls whether get_choice() clears the screen
+    user_input = 0       # Main menu user choice
 
     # MENU
     while ret_val == 0:
         # Main Menu
         try:
-            user_input = get_choice(MAIN_MENU, choice_type=int, return_choice=True)
+            user_input = get_choice(MAIN_MENU, choice_type=int, return_choice=True,
+                                    clear_screen=clear_screen)
         except (TypeError, ValueError) as err:
             print_exception(err)
             ret_val = 1
@@ -35,24 +38,31 @@ def main() -> int:
         except Exception as err:
             print_exception(err)
             ret_val = 3
-        else:
-            print(f'User chose {user_input} which is of type {type(user_input)}')  # DEBUGGING
         finally:
             if ret_val != 0:
                 break
 
         # User Selection
-        # 1: Randomize Equipment
-        if user_input == 1:
-            print_exception(NotImplementedError('The "randomize equipment" feature is not done.'))
-        # 2: Randomize Character
-        elif user_input == 2:
-            print_exception(NotImplementedError('The "randomize character" feature is not done.'))
-        # 999: Exit
-        elif user_input == 999:
-            break
-        else:
-            ret_val = 3  # How did we get here?!
+        try:
+            # 1: Randomize Equipment
+            if user_input == 1:
+                print(f'One item: {rando_db_entry(EQUIP_FOOD_DB.path_obj)}')
+                print(f'Ten items:{rando_db_entries(EQUIP_FOOD_DB.path_obj, 10)}')
+                clear_screen = False  # Don't clear the results
+            # 2: Randomize Character
+            elif user_input == 2:
+                print_exception(NotImplementedError('The "randomize character" feature is not done.'))
+            # 999: Exit
+            elif user_input == 999:
+                break
+            else:
+                ret_val = 3  # How did we get here?!
+        except (TypeError, ValueError, FileNotFoundError, LookupError) as err:
+            print_exception(err)
+            ret_val = 1
+        except Exception as err:
+            print_exception(err)
+            ret_val = 3
 
     # DONE
     return ret_val
