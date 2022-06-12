04a1a97a9d47d92468c129a91e695f8152470e27
hark130 <hark130@yahoo.com>
2022-06-12 15:22:45 -0500

    GOKI-1: Updated/added equipment databases
    Changes to be committed:
    	modified:   goki/constants.py
    	modified:   goki/databases/equipment/food.db
    	new file:   goki/databases/equipment/misc.db
    	new file:   goki/databases/equipment/tool.db
    	new file:   goki/databases/equipment/weapon.db

--- goki/constants.py
+++ goki/constants.py
@@ -16,11 +16,22 @@ Database = namedtuple('Database', 'name path_obj')
 DATABASE_DIR: Final[Path] = Path.cwd().joinpath('goki', 'databases')  # Database directory
 EQUIPMENT_DIR: Final[Path] = DATABASE_DIR.joinpath('equipment')  # Database/Equipment directory
 # Equipment/Clothing
-EQUIP_CLOTHING_DB: Final[Database] = Database('Equipment', EQUIPMENT_DIR.joinpath('clothing.db'))
+EQUIP_CLOTHING_DB: Final[Database] = Database('Clothing', EQUIPMENT_DIR.joinpath('clothing.db'))
 # Equipment/Food
-EQUIP_FOOD_DB: Final[Database] = Database('Equipment', EQUIPMENT_DIR.joinpath('food.db'))
+EQUIP_FOOD_DB: Final[Database] = Database('Food', EQUIPMENT_DIR.joinpath('food.db'))
+# Equipment/Misc
+EQUIP_MISC_DB: Final[Database] = Database('Misc', EQUIPMENT_DIR.joinpath('misc.db'))
+# Equipment/Tool
+EQUIP_TOOL_DB: Final[Database] = Database('Tool', EQUIPMENT_DIR.joinpath('tool.db'))
+# Equipment/Weapon
+EQUIP_WEAPON_DB: Final[Database] = Database('Weapon', EQUIPMENT_DIR.joinpath('weapon.db'))
 # Equipment Database List
-EQUIP_DB_LIST: Final[List[Database]] = [EQUIP_CLOTHING_DB, EQUIP_FOOD_DB]
+EQUIP_DB_LIST: Final[List[Database]] = [EQUIP_CLOTHING_DB,
+                                        EQUIP_FOOD_DB,
+                                        EQUIP_MISC_DB,
+                                        EQUIP_TOOL_DB,
+                                        EQUIP_WEAPON_DB,
+                                        ]
 
 # MENUS
 MAIN_MENU: Final = Menu('GOBLIN KING', {1: 'Randomize equipment', 2: 'Randomize a character',
